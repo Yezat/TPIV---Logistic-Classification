0d429950ae18311a272a070af706fa6c8675e28e
Kasimir Tanner <ktanner@jed.jed.cluster>
2023-10-26 20:51:56 +0200

    optimal lambdas

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 #SBATCH --chdir=/home/ktanner/TPIV---Logistic-Classification/experiments
 #SBATCH --job-name=TPIV-Adversarial
 #SBATCH --nodes=1
-#SBATCH --ntasks=50
+#SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=400G
+#SBATCH --mem=100G
 #SBATCH --output=out.txt
 #SBATCH --error=error.txt
 #SBATCH --time=36:00:00
@@ -13,6 +13,6 @@ module purge
 module load gcc openmpi python/3.10.4
 source /home/ktanner/venvs/adv/bin/activate
 
-srun --mpi=pmi2 python3 ./sweep.py
+srun --mpi=pmi2 python3 ./sweep.py sweep_experiment.json
 
 deactivate
